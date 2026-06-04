a4c2b0228c3c7d4e0fb14cf51544126211eb2cf0
Tsilavina-Doriot <winjefta85@gmail.com>
2026-06-04 09:01:05 +0300

    Refactor struct to use dynamic memory allocatione
    verifie ce codes

--- prog.c
+++ prog.c
@@ -4,73 +4,34 @@
 
 struct informations
 {
-    char nom[100];
-    char prenoms[150];
-    char niveau[10];
-    char age[20];
+    char *nom;
+    char *prenoms;
+    char *niveau;
+    char *age;
 };
 
-// void mamaky()
-// {
-//     int i = 0;
-    // struct informations e[100]; 
-
-    // FILE *mamaky = fopen("etudiant", "r");
-    // if (mamaky == NULL) {
-    //     printf("Erreur : Impossible d'ouvrir le fichier 'etudiant'.\n");
-    //     return;
-    // }
-
-    // // 1. Lecture et chargement de tout le fichier en mémoire
-    // while(i < 100 && fscanf(mamaky, "%99s %149s %19s %9s", e[i].nom, e[i].prenoms, e[i].age, e[i].niveau) == 4)
-    // {
-    //     i++;
-    // }
-    // fclose(mamaky);
-
-    // int total_etudiants = i; 
-
-    // // 2. Tri à bulles par ordre DÉCROISSANT de l'âge
-    // // On utilise atoi() car l'âge est stocké dans un char[]
-    // for (i = 0; i < total_etudiants - 1; i++)
-    // {
-    //     for (int j = 0; j < total_etudiants - i - 1; j++)
-    //     {
-    //         // Décroissant : Si l'âge de l'étudiant J est PLUS PETIT que celui de J+1, on échange
-    //         if (atoi(classe[j].age) < atoi(classe[j + 1].age)) 
-    //         {
-    //             // Échange des structures complètes
-    //             struct informations temp = e[j];
-    //             e[j] = e[j + 1];
-    //             e[j + 1] = temp;
-    //         }
-    //     }
-    // }
-
-    // // 3. Affichage du résultat trié
-    // printf("\n============= LISTE DES ÉTUDIANTS (TRIÉS PAR ÂGE DÉCROISSANT) =============");
-//     for(i = 0; i < total_etudiants; i++)
-//     {
-//         printf("\n--- Étudiant %d ---\n", i + 1);
-//         printf(" Nom     : %s\n", e[i].nom);
-//         printf(" Prénoms : %s\n", e[i].prenoms);
-//         printf(" Âge     : %s ans\n", e[i].age);
-//         printf(" Niveau  : %s\n", e[i].niveau);
-//     }
-// }
 void mamaky()
 {
     int i = 0;
-    struct informations e[100]; 
+    struct informations e[100];
+    while(i<=100)
+{
+    e[i].noms=malloc(100*sizeof(char));
+     e[i].prenoms=malloc(150*sizeof(char));
+     e[i].age=malloc(20*sizeof(char));
+     e[i].niveau=malloc(2*sizeof(char));
+    i++;
+}
 
     FILE *mamaky = fopen("etudiant", "r");
-    if (mamaky == NULL) {
+    if (mamaky == NULL) 
+    {
         printf("Erreur : Impossible d'ouvrir le fichier 'etudiant'.\n");
         return;
     }
 
     // 1. Lecture et chargement de tout le fichier en mémoire
-    while(i < 100 && fscanf(mamaky, "%99s %149s %19s %9s", e[i].nom, e[i].prenoms, e[i].age, e[i].niveau) == 4)
+    while(i < 100 && fscanf(mamaky, "%100s %150s %20s %2s", e[i].nom, e[i].prenoms, e[i].age, e[i].niveau) == 4)
     {
         i++;
     }
@@ -104,9 +65,21 @@ void mamaky()
         printf(" Âge     : %s ans\n", e[i].age);
         printf(" Niveau  : %s\n", e[i].niveau);
     }
+    while(i<=100)
+{
+    free(e[i]->nom);
+    free(e[i]->prenoms);
+    free(e[i]->age);
+    free(e[i]->niveau);
+  i++;
+}
 }
 void enregistre(struct informations *etudiant)
 {
+    etudiant->nom=malloc(100*sizeof(char));
+    etudiant->prenoms=malloc(150*sizeof(char));
+    etudiant->age=malloc(20*sizeof(char));
+    etudiant->niveau=malloc(2*sizeof(char));
     FILE *fichier = fopen("etudiant", "a");
     if (fichier == NULL) {
         printf("Erreur lors de l'ouverture du fichier\n");
@@ -115,6 +88,10 @@ void enregistre(struct informations *etudiant)
     fprintf(fichier, "%s %s %s %s\n", etudiant->nom, etudiant->prenoms, etudiant->age, etudiant->niveau);
     fclose(fichier);  
     printf("Enregistrement reussi !\n");
+    free(etudiant->nom);
+    free(etudiant->prenoms);
+    free(etudiant->age);
+    free(etudiant->niveau);
 }
 
 void inscription(struct informations *etudiant)
